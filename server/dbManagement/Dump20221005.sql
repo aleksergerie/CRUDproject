7cd0afb9f92b078f9f35d2a408b6eb79aabbe43c
Alek Sergerie <asergerie65@gmail.com>
2022-10-05 17:34:25 -0400

    added db backup in server dbManagement

---
+++ server/dbManagement/Dump20221005.sql
@@ -0,0 +1,164 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: events_db
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cities` (
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES ('Longueuil'),('Montreal'),('Quebec');
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `events` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `date_planned` timestamp NOT NULL,
+  `image` varchar(500) DEFAULT NULL,
+  `description` text NOT NULL,
+  `min_age` int DEFAULT NULL,
+  `max_participants` int DEFAULT NULL,
+  `location_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `events_chk_1` CHECK ((length(`name`) > 5)),
+  CONSTRAINT `events_chk_2` CHECK ((`min_age` > 0)),
+  CONSTRAINT `events_chk_3` CHECK ((`max_participants` > 0))
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `events`
+--
+
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+INSERT INTO `events` VALUES (1,'New in town','2022-06-05 04:00:00','https://img.freepik.com/free-photo/african-business-male-people-shaking-hands_1303-18516.jpg','New in town come make babies',16,20,1),(2,'EatFest','2022-08-09 04:00:00','https://www.mtlblog.com/media-library/image.jpg?id=26897913&width=1245&height=700&quality=85&coordinates=40%2C0%2C40%2C0','Come eat and make babies',4,21,2),(3,'Mamma be thristy','2022-09-05 04:00:00','https://www.eatthis.com/wp-content/uploads/sites/4/2022/06/woman-drinking-milk.jpg?quality=82&strip=1','Ladies night to drink milk and talk about babies',16,20,1),(4,'Basketball for elders','2022-05-27 04:00:00','https://i.pinimg.com/736x/25/12/74/25127469da48885b8d2fb6f6437afcb4--basketball-photography-sport-photography.jpg','If you are old and not able to walk, this activity is for you. Come play basketball and attract old woman',65,16,2);
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `events_users`
+--
+
+DROP TABLE IF EXISTS `events_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `events_users` (
+  `event_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`event_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `events_users`
+--
+
+LOCK TABLES `events_users` WRITE;
+/*!40000 ALTER TABLE `events_users` DISABLE KEYS */;
+INSERT INTO `events_users` VALUES (1,1),(2,1);
+/*!40000 ALTER TABLE `events_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `street` varchar(300) NOT NULL,
+  `house_number` varchar(15) NOT NULL,
+  `postal_code` varchar(15) NOT NULL,
+  `city_name` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'POPTY','Nantel','5A','J4E6T4','Longueuil'),(2,NULL,'Belmont','5687','H8G6T7','Quebec');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `birthdate` date NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Serge','Leblanc','1989-06-06','serge@gmail.ca','password'),(2,'Bobby','Walker','1979-05-23','bobby@gmail.com','password'),(3,'Alek','Sergerie','2019-01-09','sergeriea@gmail.ca','password'),(6,'Alek','Sergerie','2022-09-01','sergeria@gmail.ca','password');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-05 17:26:00
